82491ad31046c810aa06761124d3a2d08a9701f7
Huy100720 <huyemz123@gmail.com>
2020-11-19 08:35:15 +0700

    use recurstion and GREEN again

--- src/fu/util/MathUtility.java
+++ src/fu/util/MathUtility.java
@@ -15,18 +15,29 @@ public class MathUtility {
     // neu qua 20 tran kieu long
     // 0! = 1; 1! = 1; n! = 1.2.3...n 
     
-    public static long getFactorial(int n){
+//    public static long getFactorial(int n){
+//       if(n < 0 || n > 20 )
+//           throw new IllegalArgumentException("n must be >= 0 and <= 20");
+//       // đưa đầu vào cà chớn, mình ko thèm trả về giá trị ám chỉ sai
+//       if(n == 0 || n == 1)
+//           return 1;
+//       long result  =  1;
+//        for (int i = 2; i <= n; i++) {
+//            result *= i;
+//        }
+//        return result;
+//        
+//    } 
+    
+    
+     public static long getFactorial(int n){
        if(n < 0 || n > 20 )
            throw new IllegalArgumentException("n must be >= 0 and <= 20");
        // đưa đầu vào cà chớn, mình ko thèm trả về giá trị ám chỉ sai
        if(n == 0 || n == 1)
            return 1;
-       long result  =  1;
-        for (int i = 2; i <= n; i++) {
-            result *= i;
-        }
-        return result;
-        
+       
+       return  n * getFactorial(n - 1); // gọi lại chính mình với quy mô nhỏ hơn
     } 
     // Math.sqrt , .pow(a,b) a^b , abs() trị tuyệt đối , sin() 
     
